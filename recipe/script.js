e103a66b971f8503fe099784aa9a122e5effd9e0
Arielthecoder <ahermitt02@gmail.com>
2023-07-28 15:21:48 -0400

    updating js to make stars clickable

--- recipe/script.js
+++ recipe/script.js
@@ -22,3 +22,36 @@ const domLoaded = () => {
   toggleButton.addEventListener("click", () => {
     toggleContent.classList.toggle("show");
   })
+
+  
+
+const stars = document.querySelectorAll('.star');
+const ratingMessage = document.querySelector('.recipe-rating p');
+
+let selectedRating = 0;
+
+stars.forEach((star, index) => {
+  star.addEventListener('click', () => {
+    selectedRating = index + 1;
+    updateStars();
+    ratingMessage.textContent = `Thank you for rating! You rated this recipe ${selectedRating} stars.`;
+  });
+
+  star.addEventListener('mouseover', () => {
+    updateStars(index);
+  });
+
+  star.addEventListener('mouseout', () => {
+    updateStars();
+  });
+});
+
+function updateStars(index) {
+  stars.forEach((star, i) => {
+    if (i <= index) {
+      star.classList.add('active');
+    } else {
+      star.classList.remove('active');
+    }
+  });
+}

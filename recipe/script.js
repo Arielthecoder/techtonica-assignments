e61e87fd83bbe544cb559efc0a206eec12a9a8f7
Arielthecoder <ahermitt02@gmail.com>
2023-07-28 04:05:24 -0400

    fixed line-through

--- recipe/script.js
+++ recipe/script.js
@@ -1,10 +1,18 @@
-const boxes = document.querySelectorAll("input[type=checkbox]");
+const domLoaded = () => {
 
-boxes.forEach(checkbox =>{
-    checkbox.addEventListener("click", crossOut)
-});
-
-function crossOut(event) {
-    const parentLabel = event.target.parentElement;
-    parentLabel.classList.toggle('checked')
-}
+    const boxes = document.querySelectorAll("input[type=checkbox]");
+  
+    boxes.forEach(checkbox => {
+      checkbox.addEventListener("click", crossOut)
+    });
+  
+  } 
+  
+  function crossOut(event) {
+    const checkbox = event.target;
+    const parentLabel = checkbox.parentElement;
+    parentLabel.classList.toggle("checked", checkbox.checked);
+  }
+  
+  document.addEventListener('DOMContentLoaded', domLoaded);
+  

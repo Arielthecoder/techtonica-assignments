16646c667cdf53f801d554126ce3fc6f4af3093d
Arielthecoder <ahermitt02@gmail.com>
2023-08-11 16:34:41 -0400

    adding the moles

--- firstgame/mariogame.js
+++ firstgame/mariogame.js
@@ -1,3 +1,5 @@
+let currMole;
+
 window.onload = function() {
     setStart();
 }
@@ -8,4 +10,21 @@ function setStart() {
         tile.id = i.toString();
         document.getElementById("gamearea").appendChild(tile);
     }
+    setInterval(moveMole, 2000)
+}
+function randomTiles(){
+    let num = Math.floor(Math.random()*9);
+    return num.toString();
+}
+
+function moveMole(){
+    if (currMole){
+        currMole.innerHTML = ''
+    }
+    let mole = document.createElement("img");
+    mole.src = "./mole.png";
+
+    let num = randomTiles();
+    currMole = document.getElementById(num);
+    currMole.appendChild(mole);
 }
